94ae91e6f4ad45e304b15478103d2a9efc61ae3f
gpynes <gpynes@gmail.com>
2017-06-16 15:22:49 +0200

    Creates microservice

---
+++ server.js
@@ -0,0 +1,47 @@
+// const restify = require('restify'),
+const express = require('express'),
+      fs = require('fs'),
+      server = express()
+
+let urls = {},
+    id = 0
+
+
+server.get('/', (req, res) => {
+    res.write(fs.readFileSync(`${__dirname}/index.html`, 'utf-8'))
+    res.end()
+})
+
+server.get('/:id', (req, res) => {
+    const url = urls[req.params.id]
+
+    if (url) {
+        res.redirect(url)
+        return
+    }
+    res.status(500)
+    res.json({"error": "This url is not on the database."})
+    return
+})
+
+const validUrl = new RegExp(/^(http:\/\/www\.|https:\/\/www\.)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/)
+
+server.get('/new/*', (req, res) => {
+  const url = req.params[0]
+  if (!validUrl.test(url)) {
+    res.status(500).json({error: 'not a valid url'})
+      return
+  }
+  if (!urls[url]) {
+    urls[url] = id
+    urls[id] = url
+    id++
+  }
+
+  res.json({
+    original_url: url,
+    short_url:  `${req.hostname}/${urls[url]}`
+  })
+})
+
+server.listen(process.env.PORT || 8080, () => console.log(`Server listening on port ${process.env.PORT || 8080}`))
